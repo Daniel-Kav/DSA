f2f954a6b673aa5de717129c1649587310b52f97
daniel-kav <dankavatha01@gmail.com>
2023-09-19 23:06:23 +0300

    aadded a node factory and a linked list factory

--- linked_list.js
+++ linked_list.js
@@ -1,1 +1,36 @@
-//linked list factory
+//linked list factory/class
+class LinkedList{
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.nextNode = newNode;
+            this.tail = newNode;
+        }
+    }
+
+    prepend(value){
+        const newNode = new Node(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            newNode.nextNode = this.head;
+            this.head = newNode;
+        }
+    }
+}
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.newNode = null;
+    }
+}
